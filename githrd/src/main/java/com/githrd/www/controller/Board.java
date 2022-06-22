50b1cb43bcd1bd8f889fcf441a53eeb54e3b1044
Soyeon Kim <100174595+yeonnns@users.noreply.github.com>
2022-06-22 23:58:38 +0900

    file board insert add

--- githrd/src/main/java/com/githrd/www/controller/Board.java
+++ githrd/src/main/java/com/githrd/www/controller/Board.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.githrd.www.dao.BoardDao;
+import com.githrd.www.service.BoardService;
 import com.githrd.www.util.PageUtil;
 import com.githrd.www.vo.BoardVO;
 import com.githrd.www.vo.FileVO;
@@ -24,17 +25,22 @@ import com.githrd.www.vo.FileVO;
  * 				2022.06.17 - 담당자 : 김소연
  * 							클래스 제작
  * 							1) 게시글 리스트 보기 요청 처리 함수 제작
+ * 
+ * 				2022.06.22 - 담당자 : 김소연
+ * 							게시글 등록 요청 처리 함수 추가
 */
 @Controller
 @RequestMapping("/board")
 public class Board {
 	@Autowired
 	BoardDao bDao;
+	@Autowired
+	BoardService bSrvc;
 	
 	// 게시글 리스트 페이지 요청 처리함수
 	@RequestMapping("/boardList.blp")
 	public ModelAndView boardList(ModelAndView mv, PageUtil page) {
-		System.out.println("########## nowPage : " + page.getNowPage());
+//		System.out.println("########## nowPage : " + page.getNowPage());
 		// 할일
 		// 1. 총 게시글 수 가져오고
 		int total = bDao.getTotal();
@@ -66,6 +72,7 @@ public class Board {
 		// 뷰 정하고
 		mv.setViewName("board/boardDetail");
 		return mv;
+		
 	}
 	
 	// 게시판 글쓰기 폼보기요청 처리함수
@@ -74,4 +81,28 @@ public class Board {
 		mv.setViewName("board/boardWrite");
 		return mv;
 	}
+	
+	// 게시글 등록 요청 처리함수
+	@RequestMapping("/boardWriteProc.blp")
+	public ModelAndView boardWriteProc(ModelAndView mv, BoardVO bVO, String nowPage) {
+		String view = "/www/board/boardList.blp";
+		System.out.println("************ " + bVO);
+		try {
+			bSrvc.addBoardData(bVO);
+			nowPage = "1";
+		} catch(Exception e) {
+			// 게시글 등록에 실패한 경우
+			// 결과적으로 롤백된 경우....
+//			view = "/www/board/boardWrite.blp?nowPage=" + nowPage;
+			view = "/www/board/boardWrite.blp";
+			e.printStackTrace();
+		}
+		
+		mv.addObject("NOWPAGE", nowPage);
+		mv.addObject("VIEW", view);
+		
+		mv.setViewName("board/redirect");
+		return mv;
+	}
+	
 }
