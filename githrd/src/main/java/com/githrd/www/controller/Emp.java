74962bd3ea6fb875ad4bc24491694b5a7d697bef
Soyeon Kim <100174595+yeonnns@users.noreply.github.com>
2022-06-29 22:03:51 +0900

    selectList initial 회원 조회 add

--- githrd/src/main/java/com/githrd/www/controller/Emp.java
+++ githrd/src/main/java/com/githrd/www/controller/Emp.java
@@ -1,48 +1,66 @@
 package com.githrd.www.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
 
-import com.githrd.www.dao.EmpDao;
-import com.githrd.www.vo.EmpVO;
+import com.githrd.www.dao.*;
+import com.githrd.www.vo.*;
 
 @Controller
 @RequestMapping("/emp")
 public class Emp {
 	@Autowired
 	EmpDao eDao;
-	// 사원 페이지 폼보기 요청 처리 함수
+	
+	// 사원 페이지 폼보기 요청 처리함수
 	@RequestMapping("/empPage.blp")
 	public ModelAndView empPage(ModelAndView mv) {
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("부 서");
 		list.add("직 급");
 		
+		List<EmpVO> iList = eDao.getIniList();
 		// 데이터 전달하고
 		mv.addObject("LIST", list);
+		mv.addObject("ILIST", iList);
 		mv.setViewName("emp/empPage");
 		return mv;
 	}
-
-	// 분류 리스트 요청 처리 함수
+	
+	// 분류 리스트 요청 처리함수
 	@RequestMapping("/selList.blp")
 	@ResponseBody
-	public List<EmpVO> selList(EmpVO eVO) {
-		List<EmpVO> list = 	eDao.selList(eVO);
+	public List<EmpVO> selList(EmpVO eVO){
+		List<EmpVO> list = eDao.selList(eVO);
 		return list;
 	}
-
-	// 분류별 사원 리스트 요청 처리 함수
+	
+	// 분류 리스트 요청 처리함수
 	@RequestMapping("/partList.blp")
 	@ResponseBody
-	public List<EmpVO> partList(EmpVO eVO) {
-		List<EmpVO> list = 	eDao.partList(eVO);
+	public List<EmpVO> partList(EmpVO eVO){
+		List<EmpVO> list = eDao.partList(eVO);
 		return list;
 	}
+	
+	// 사원 정보 조회 요청 처리함수
+	@RequestMapping("/empInfo.blp")
+	@ResponseBody
+	public EmpVO empInfo(EmpVO eVO) {
+		eVO = eDao.getInfo(eVO);
+		return eVO;
+	}
+	
+	// 이니셜로 이름 리스트 조회 요청 처리함수
+	@RequestMapping("/enameList.blp")
+	@ResponseBody
+	public List<EmpVO> enameList(EmpVO eVO){
+		List<EmpVO> list = eDao.nameList(eVO);
+		return list;
+	}
+	
 }
