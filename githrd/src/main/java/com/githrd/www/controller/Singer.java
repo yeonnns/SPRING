8c77508be0d348db35886829b1d863ed29d2716a
Soyeon Kim <100174595+yeonnns@users.noreply.github.com>
2022-07-03 22:27:34 +0900

    singMemb add

--- githrd/src/main/java/com/githrd/www/controller/Singer.java
+++ githrd/src/main/java/com/githrd/www/controller/Singer.java
@@ -1,17 +1,38 @@
 package com.githrd.www.controller;
 
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.*;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.githrd.www.dao.SingDao;
+import com.githrd.www.vo.SingVO;
 
 @Controller
 @RequestMapping("/singer")
 public class Singer {
-	
+	@Autowired
+	SingDao siDao;
 	@RequestMapping("/singer.blp")
 	public ModelAndView singer(ModelAndView mv) {
 		mv.setViewName("singer/singer");
 		return mv;
 	}
-
+	// 가수 선택
+	@RequestMapping("/singList.blp")
+	@ResponseBody
+	public List<SingVO> singList(SingVO siVO) {
+		List<SingVO> list = siDao.singList(siVO);
+		return list;
+	}
+	// 멤버 선택
+	@RequestMapping("/singMemb.blp")
+	@ResponseBody
+	public List<SingVO> singMemb(SingVO siVO) {
+		List<SingVO> list = siDao.singMemb(siVO);
+		return list;
+	}
 }
