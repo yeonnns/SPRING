9ef4f677b7ffd4050c2fe39294984b3491a17c7f
Soyeon Kim <100174595+yeonnns@users.noreply.github.com>
2022-07-06 22:41:36 +0900

    product add

---
+++ githrd/src/main/java/com/githrd/www/controller/Product.java
@@ -0,0 +1,37 @@
+package com.githrd.www.controller;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
+
+import com.githrd.www.dao.*;
+import com.githrd.www.vo.*;
+
+@Controller
+@RequestMapping("/cate")
+public class Product {
+	@Autowired
+	ProductDao pDao;
+	
+	@RequestMapping("/product.blp")
+	public ModelAndView productMain(ModelAndView mv, ProductVO pVO) {
+		List<ProductVO> list = pDao.getCateList(pVO);
+		
+		// 데이터 심고
+		mv.addObject("LIST", list);
+		// 뷰 부르고
+		mv.setViewName("cate/product");
+		return mv;
+	}
+	
+	@RequestMapping("/cateList.blp")
+	@ResponseBody
+	public List<ProductVO> cateList(ProductVO pVO){
+		List<ProductVO> list = pDao.getCateList(pVO);
+		
+		return list;
+	}
+}
